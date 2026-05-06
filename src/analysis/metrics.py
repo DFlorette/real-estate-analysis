7527baf4063b4e8a81fbe42b72edcabaacd6d566
DFlorette <3fH9YSRP43>
2026-05-06 16:57:03 +0200

    feat: getting the core market from a Dataframe

--- src/analysis/metrics.py
+++ src/analysis/metrics.py
@@ -1,8 +1,8 @@
 import numpy as np
+import pandas as pd
 
-def detect_outliers(df):
-    q1 = df["prix_m2"].quantile(0.25)
-    q3 = df["prix_m2"].quantile(0.75)
-    iqr = q3 - q1
 
-    return df[(df["prix_m2"] < q1 - 1.5 * iqr) | (df["prix_m2"] > q3 + 1.5 * iqr)]
+def get_core_market(df: pd.DataFrame, lower: float = 0.1, upper: float = 0.9) -> pd.DataFrame:
+    q_low = df["prix_m2"].quantile(lower)
+    q_high = df["prix_m2"].quantile(upper)
+    return df[(df["prix_m2"] >= q_low) & (df["prix_m2"] <= q_high)]
