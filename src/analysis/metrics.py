78cff94ce73006c63b946f5de78941e1e37edaf9
DFlorette <3fH9YSRP43>
2026-05-06 17:15:10 +0200

    refactor: metrics.py

--- src/analysis/metrics.py
+++ src/analysis/metrics.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 
-
 def get_core_market(df: pd.DataFrame, lower: float = 0.1, upper: float = 0.9) -> pd.DataFrame:
     q_low = df["prix_m2"].quantile(lower)
     q_high = df["prix_m2"].quantile(upper)
